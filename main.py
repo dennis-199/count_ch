2252d9f48f764ffdef1c60963dc3b0256d37a2ee
dennis-199 <otieno.dennis@strathmore.edu>
2021-09-19 13:19:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Count the number of characters in string str1. Do not use len(). Save the number in variable numbs.
+str1 = "I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living."
+numbs = 0
+for i in str1:
+    numbs = numbs + 1
+    print(numbs)
+
